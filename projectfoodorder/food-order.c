f4d0d5667cf2295296b30a67eadae90d3053b2ba
Aum3010 <aumyp2002@gmail.com>
2023-12-15 16:02:22 +0530

    Added documentation changes 2

--- projectfoodorder/food-order.c
+++ projectfoodorder/food-order.c
@@ -1,3 +1,506 @@
+--Documentation
+- File: [File Name]
+- Overview:
+This file contains the implementation of a restaurant order delivery system. It includes functions for creating and managing a queue of food orders.
+
+- Functions/Methods:
+1. createque:
+   - Description: Creates a new queue with the given size.
+   - Parameters: 
+     - actual_size (unsigned): The size of the queue.
+   - Returns: A pointer to the created queue.
+
+2. isempty:
+   - Description: Checks if the queue is empty.
+   - Parameters:
+     - que (struct que*): A pointer to the queue.
+   - Returns: 1 if the queue is empty, 0 otherwise.
+
+3. enque:
+   - Description: Adds an item to the queue.
+   - Parameters:
+     - que (struct que*): A pointer to the queue.
+     - item (int): The item to be added to the queue.
+   - Returns: None.
+
+4. isfull:
+   - Description: Checks if the queue is full.
+   - Parameters:
+     - que (struct que*): A pointer to the queue.
+   - Returns: 1 if the queue is full, 0 otherwise.
+
+5. deque:
+   - Description: Removes an item from the queue.
+   - Parameters:
+     - que (struct que*): A pointer to the queue.
+   - Returns: The item removed from the queue.
+
+6. display:
+   - Description: Displays the items in the queue.
+   - Parameters:
+     - que (struct que*): A pointer to the queue.
+   - Returns: None.
+
+7. main:
+   - Description: The main function of the program.
+   - Parameters: None
+   - Returns: 0
+
+--End of documentation
+
+// headers for functionalities like current time console in out and system commands
+#include <stdio.h>
+#include <stdlib.h>
+#include<limits.h>
+#include<time.h>
+#include<conio.h>
+
+// global variables
+int a,i,j,item;
+float total_1,total_2,total_3,total_4,total_5,total_6,total_7,total_8,total_9,total_10,total_11,total_12,total_13,total_14,total_15;
+
+
+// struct define
+struct que{
+int front, rear ,size;
+unsigned actual_size;
+float *arr;
+
+};
+
+
+
+struct que* createque(unsigned actual_size)
+{
+ struct que* que =(struct que*)malloc(sizeof(struct que));
+que->actual_size= actual_size;
+que->front =que->rear=-1;
+que->size=0;
+que->arr=(int*)malloc(que->actual_size*sizeof(int));
+return que;
+};
+
+
+int isempty(struct que*que)
+{
+    return(que->size=0);
+
+}
+
+
+void enque(struct que*que,int item)
+{
+     if(isfull(que))
+        return;
+     que->rear=que->rear+1;
+     que->arr[que->rear]= item;
+     que->size=que->size+1;
+     if(que->front== -1)
+        que->front++;
+     printf("%d item added to queue \n",item);
+}
+
+int isfull(struct que*que)
+{
+    return(que->size==que->actual_size);
+}
+
+
+int  deque(struct que* que)
+{
+    if(isempty(que))
+     return INT_MIN;
+    int item =que->arr[que->front];
+    if(que->front==que->rear){
+        que->front =que->rear=-1;
+
+    }
+    else
+        que->front=que->front+1;
+    que->size=que->size-1;
+
+
+
+printf("\n=========== BILL ========================          ");
+switch(item)
+{
+case 1:
+    printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_1);
+     break ;
+case 2:
+    printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_2);
+    break ;
+case 3:
+    printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_3);
+    break ;
+
+case 4:
+    printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_4);
+    break ;
+
+case 5:
+      printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_5);
+      break ;
+
+case 6:
+       printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_6);
+       break ;
+
+case 7:
+       printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_7);
+       break ;
+case 8:
+       printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_8);
+       break ;
+case 9:
+       printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_9);
+       break ;
+case 10:
+       printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_10);
+       break ;
+case 11:
+       printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_11);
+       break ;
+case 12:
+       printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_12);
+       break ;
+case 13:
+       printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_13);
+       break ;
+case 14:
+       printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_14);
+       break ;
+case 15:
+       printf("\nYour total bill amount for order %d is Rs.%.2f,\nPayment: CASH\n",item,total_15);
+       break ;
+default:
+    printf("\nnot yet ordered...order first\n");
+}
+printf("\n order %d is deleted \n",item);
+printf("===================================          ");
+    return item;
+
+
+}
+
+
+
+
+void display(struct que*que)
+{
+
+   if(isempty(que))
+    printf("__queue is empty__");
+    printf("\n>>> Displaying food orders in a queue");
+     for(item=que->front;item<=que->rear;item++)
+     printf("\n=====order queue is %.f======",que->arr[item]);
+
+}
+
+
+
+
+
+int main()
+{
+   int val,n,quantity;
+
+   struct que*que =createque(1000);
+   do
+   {      printf("\n");
+printf("            ##################################        \n");
+printf("                 Restaurant Order Delivery              \n");
+printf("            ##################################        \n");
+  time_t t;   // not a prim
+
+--Documentation
+
+This code is a part of a program that manages an order queue for a food delivery service. It allows users to add orders, delete orders and generate a bill, and display the current orders queue. The code includes a menu of food items and their prices, and prompts the user to select a food item and enter the quantity. It then calculates the total price of the order and adds it to the orders queue.
+
+- Function/Method:
+- **Name**: main
+- **Description**: The main function of the program that handles the user interaction and order management.
+- **Parameters**: None
+- **Returns**: 0 if the program executes successfully
+
+--Actual Code
+
+```c
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+  time_t t;
+  int n, a, quantity;
+  int total_1, total_2, total_3, total_4, total_5, total_6;
+  
+  time(&t);
+  printf("\t\t %s", ctime(&t));
+  printf("             _________________________________         \n");
+
+  printf("[1].Add an order:\n");
+  printf("[2].delete an order and to get bill:\n");
+  printf("[3].display orders queue:\n");
+  printf("[4].exit\n");
+  printf("Please enter your choice: ");
+  scanf("%d", &n);
+
+  switch(n) {
+    case 1:
+      printf("\t        ==============================          \n");
+      printf("\t                    Food Menu   \n");
+      printf("\t        ==============================          \n");
+      printf("..BREAKFAST MENU..... \n");
+      printf("[1] Pancakes ------------------------------------------- Rs.210 \n");
+      printf("[2] Veg. Sandwich--------------------------------------- Rs.199 \n");
+      printf("[3] Aloo Paratha---------------------------------------- Rs.185 \n");
+      printf("[4] Dhokla-----------------------------------------------Rs.135  \n");
+      printf("[5] Poha------------------------------------------------- Rs.170 \n");
+      printf("..Lunch Menu.........   \n");
+      printf("[6] Paneer Butter Masala with paratha--------------------Rs. 275 \n");
+      printf("[7] Masala Dosa------------------------------------------ Rs.299 \n");
+      printf("[8]  Chicken -------------------------------------------- Rs.320 \n");
+      printf("[9] Sausage and Potato Casserole------------------------- Rs.400 \n");
+      printf("[10] Spring Rolls---------------------------------------- Rs.365 \n");
+      printf("..Dinner Menu........   \n");
+      printf("[11] Veg  Manchurian------------------------------------- Rs.260 \n");
+      printf("[12] Chili Chicken Tikka--------------------------------- Rs.460 \n");
+      printf("[13] Veg Biryani----------------------------------------- Rs.275 \n");
+      printf("[14] Egg cury with Rice --------------------------------- Rs.350 \n");
+      printf("[15] Sausage Burger-------------------------------------- Rs.300 \n");
+      printf("\tChoose your food item: ");
+      scanf("%d", &a);
+
+      switch(a) {
+        case 1:
+          printf("\nIngredient : All-Purpose Flour,Sugar,Baking Powder,Teaspoon Salt,Ground Nutmeg,Eggs,Unsalted Butter,\n\n");
+          printf("\tEnter Quantity : ");
+          scanf("%d", &quantity);
+          total_1 = 210 * quantity ;
+          enque(que, a);
+          break;
+        case 2:
+          printf("\nIngredient : Bread slices, Boiled sweet corn, chopped Onions and Capsicums, Mayonaise, Butter, Oregano, Red chilli flakes, Processed Cheese\n\n");
+          printf("Enter Quantity : ");
+          scanf("%d", &quantity);
+          total_2 =  199 * quantity ;
+          enque(que, a);
+          break ;
+        case 3:
+          printf("\nIngredient : Mashed potatoes, Chopped Onions and coriander, Ginger-green chilli paste, Flour, Salt, Garam Masala\n\n");
+          printf("Enter Quantity : ");
+          scanf("%d", &quantity);
+          total_3 =  185 * quantity ;
+          enque(que, a);
+          break;
+        case 4:
+          printf("\nIngredient : Atta, Baking soda, Vegetable oil, Ginger, Green chilli, Red chilli, Ajwain, Yogurt, Garlic, Salt \n\n");
+          printf("Enter Quantity : ");
+          scanf("%d", &quantity);
+          total_4 = 135 * quantity ;
+          enque(que, a);
+          break ;
+        case 5:
+          printf("\nIngredient : Poha, Onion, Oil, Peanuts, Salt, Turmeric Powder, Green chilli, Mustard seeds, Curry leaves\n\n");
+          printf("Enter Quantity : ");
+          scanf("%d", &quantity);
+          total_5 = 170 * quantity ;
+          enque(que, a);
+          break ;
+        case 6:
+          printf("\nIngredient : Paneer, Onion and tomatoes, Cashews, Turmeric, Sugar, Cloves, Ginger, Salt, Red chilli, Cream, Cinnamon, Coriandar powder, Garam masala \n\n");
+          printf("Enter Quantity : ");
+          scanf("%d", &quantity);
+          total_6 = 275 * quantity ;
+          enque(que, a);
+          break;
+      }
+      break;
+  }
+
+  return 0;
+}
+```
+
+# Code Documentation Template
+-- File: [File Name]
+- Overview:
+This code represents a menu system for a restaurant. It allows the user to select a dish from the menu, enter the quantity, and calculate the total cost. The user can also view the current order, remove items from the order, and exit the program.
+
+- Functions/Methods:
+1. enque(que, a):
+- Description: Adds an item to the queue.
+- Parameters: 
+  - que: The queue data structure.
+  - a: The item to be added to the queue.
+- Returns: None
+
+2. isfull(que):
+- Description: Checks if the queue is full.
+- Parameters: 
+  - que: The queue data structure.
+- Returns: None
+
+3. isempty(que):
+- Description: Checks if the queue is empty.
+- Parameters: 
+  - que: The queue data structure.
+- Returns: None
+
+4. deque(que):
+- Description: Removes an item from the queue.
+- Parameters: 
+  - que: The queue data structure.
+- Returns: None
+
+5. display(que):
+- Description: Displays the current items in the queue.
+- Parameters: 
+  - que: The queue data structure.
+- Returns: None
+
+6. main():
+- Description: The main function that drives the program.
+- Parameters: None
+- Returns: None
+
+--End of documentation
+
+# File: [File Name]
+
+# Overview:
+# This file contains code for a program that converts a file into a different format. It includes a main function that calls other functions to perform the conversion.
+
+# Function/Method 1
+# Name: getch
+# Description: This function waits for a user input.
+# Parameters: None
+# Returns: The input character
+
+# Function/Method 2
+# Name: display
+# Description: This function displays the contents of a queue.
+# Parameters: 
+# - que (queue): The queue to display
+# Returns: None
+
+# Function/Method 3
+# Name: convert_format
+# Description: This function converts a file to a different format.
+# Parameters: 
+# - file_path (string): The path to the input file
+# - format (string): The desired output format
+# Returns: The filename of the converted file
+
+# Function/Method 4
+# Name: main
+# Description: This is the main function of the program. It calls other functions to convert a file to a different format and displays a message.
+# Parameters: None
+# Returns: None
+
+def getch():
+    while ('y' == getch()):
+        display(que)
+    printf("\n\n\t=================FOS=================          ")
+    printf("   \n         Thanks for choosing our restaurant\n\n")
+    printf("             We hope to see you again!  \n")
+    printf("\t======================================  \n")
+
+def display(que):
+    # Display the contents of the queue
+    pass
+
+def convert_format(file_path, format):
+    # Convert the file to the specified format
+    pass
+
+def main():
+    # Main function of the program
+    pass
+
+-- File: food-order.c
+- Overview:
+This file contains the implementation of a food ordering system. It includes functions for adding items to the order, calculating the total cost, and displaying the order details.
+
+- Functions/Methods:
+
+1. add_item_to_order:
+   - Description: Adds an item to the order.
+   - Parameters:
+     - item_name (string): The name of the item to add.
+     - price (float): The price of the item.
+   - Returns: None
+
+2. calculate_total_cost:
+   - Description: Calculates the total cost of the order.
+   - Parameters: None
+   - Returns: The total cost of the order (float).
+
+3. display_order_details:
+   - Description: Displays the details of the order, including the items and their prices.
+   - Parameters: None
+   - Returns: None
+
+-- Actual Code:
+```c
+#include <stdio.h>
+
+#define MAX_ITEMS 10
+
+char items[MAX_ITEMS][50];
+float prices[MAX_ITEMS];
+int num_items = 0;
+
+// Adds an item to the order.
+void add_item_to_order(char item_name[], float price) {
+    if (num_items < MAX_ITEMS) {
+        strcpy(items[num_items], item_name);
+        prices[num_items] = price;
+        num_items++;
+    }
+}
+
+// Calculates the total cost of the order.
+float calculate_total_cost() {
+    float total_cost = 0.0;
+    for (int i = 0; i < num_items; i++) {
+        total_cost += prices[i];
+    }
+    return total_cost;
+}
+
+// Displays the details of the order, including the items and their prices.
+void display_order_details() {
+    printf("Order Details:\n");
+    for (int i = 0; i < num_items; i++) {
+        printf("%s - $%.2f\n", items[i], prices[i]);
+    }
+}
+
+int main() {
+    // Usage example
+    add_item_to_order("Pizza", 9.99);
+    add_item_to_order("Burger", 5.99);
+    add_item_to_order("Fries", 2.99);
+    
+    display_order_details();
+    
+    float total_cost = calculate_total_cost();
+    printf("Total Cost: $%.2f\n", total_cost);
+    
+    return 0;
+}
+```
+
+- Usage Example:
+```
+Order Details:
+Pizza - $9.99
+Burger - $5.99
+Fries - $2.99
+Total Cost: $18.97
+```
+
 // headers for functionalities like current time console in out and system commands
 #include <stdio.h>
 #include <stdlib.h>
